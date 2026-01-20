4843ed0f6405464e41cb6f221923f8aec4824bb5
Yodhan722 <yodhan.orre@gmail.com>
2026-01-20 16:41:50 -0500

    Update canvas size to window dimensions

--- demo.js
+++ demo.js
@@ -1,5 +1,12 @@
+// Minimal p5.js demo showing mouse coordinates near the cursor
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(windowWidth, windowHeight);
+  textSize(14);
+  Stroke(1);
+}
+
+function draw() {
+  background(255);
   fill(0, 0, 0);
-  rect(0, 0, 600, 400);
+  text(`${mouseX},${mouseY}`, mouseX - 20, mouseY - 20);
 }
