288b7d9789a875751d6ef2283c18acb4b2c057c8
Yodhan722 <yodhan.orre@gmail.com>
2026-01-22 18:51:55 -0500

    a

--- roguelite_game.js
+++ roguelite_game.js
@@ -1,17 +1,14 @@
 var programCode = function(processingInstance) {
 with (processingInstance) {
-      size(600, 400); 
-      frameRate(30);
+    size(600, 400); 
+    frameRate(30);
         
-      fill(255, 255, 0);
-      ellipse(200, 200, 200, 200);
-      noFill();
-      stroke(0, 0, 0);
-      strokeWeight(2);
-      arc(200, 200, 150, 100, 0, PI);
-      fill(0, 0, 0);
-      ellipse(250, 200, 10, 10);
-      ellipse(153, 200, 10, 10);
+    function drawPlayer() {
+        var x = 300;
+        var y = 200;
+
+        rect(x, y, 70, 70);
+    }
 }};
 
 var canvas = document.getElementById("rogueliteCanvas"); 
