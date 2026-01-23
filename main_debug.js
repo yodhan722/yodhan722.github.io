fa1a070d22216f2f5a99ca346a5ac95c539f8206
Yodhan722 <yodhan.orre@gmail.com>
2026-01-23 13:40:38 -0500

    a

--- main_debug.js
+++ main_debug.js
@@ -1,18 +1,19 @@
 var programCode = function(processingInstance) {
     with (processingInstance) {
-    size(600, 400); 
-    frameRate(30);
+        size(600, 400); 
+        frameRate(30);
+        stroke(1);
         
-    fill(255, 255, 0);
-    ellipse(200, 200, 200, 200);
-    noFill();
-    stroke(0, 0, 0);
-    strokeWeight(2);
-    arc(200, 200, 150, 100, 0, PI);
-    fill(0, 0, 0);
-    ellipse(250, 200, 10, 10);
-    ellipse(153, 200, 10, 10);
-}
+        fill(255, 255, 0);
+        ellipse(200, 200, 200, 200);
+        noFill();
+        stroke(0, 0, 0);
+        strokeWeight(2);
+        arc(200, 200, 150, 100, 0, PI);
+        fill(0, 0, 0);
+        ellipse(250, 200, 10, 10);
+        ellipse(153, 200, 10, 10);
+    }
 };
 
 var canvas = document.getElementById("mycanvas"); 
