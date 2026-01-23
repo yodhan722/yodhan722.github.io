90399ea76ccc31db23fa97f82bb6e6100aa2b0b0
Yodhan722 <yodhan.orre@gmail.com>
2026-01-23 13:05:30 -0500

    a

--- main_debug.js
+++ main_debug.js
@@ -1,2 +1,19 @@
-console.log("Welcome to my website.");
-console.log("This is where the debug console.");
+var programCode = function(processingInstance) {
+    with (processingInstance) {
+    size(400, 400); 
+    frameRate(30);
+        
+    fill(255, 255, 0);
+    ellipse(200, 200, 200, 200);
+    noFill();
+    stroke(0, 0, 0);
+    strokeWeight(2);
+    arc(200, 200, 150, 100, 0, PI);
+    fill(0, 0, 0);
+    ellipse(250, 200, 10, 10);
+    ellipse(153, 200, 10, 10);
+}
+};
+
+var canvas = document.getElementById("mycanvas"); 
+var processingInstance = new Processing(canvas, programCode); 
