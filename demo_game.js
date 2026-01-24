7c5380e7fff651f0a9c4f5fe62425735c6dd4dc9
Yodhan722 <yodhan.orre@gmail.com>
2026-01-23 21:19:06 -0500

    hh

--- demo_game.js
+++ demo_game.js
@@ -3,17 +3,45 @@ var programCode = function(processingInstance) {
         size(600, 400); 
         frameRate(30);
         
+        // the health for the player
+        var health = 20;
+    
+        // the x and y pos for the player
+        var x = 300;
+        var y = 200;
+
+        background(56, 225, 89);
         stroke(1);
-        
-        fill(255, 255, 0);
-        ellipse(200, 200, 200, 200);
-        noFill();
-        arc(200, 200, 150, 100, 0, 180);
+        textSize(20);
+
+        draw = function() {
+            background(56, 225, 89);
+    
+        // the text for heath for the player
         fill(0, 0, 0);
-        ellipse(250, 200, 10, 10);
-        ellipse(153, 200, 10, 10);
-    }
-};
+        text("health: " + health, 0, 25);
+    
+        // The player ball
+        fill(64, 138, 235);
+        ellipse(x, y, 50, 50);
+    
+        // the player movement up, down, left, and right
+        if(keyIsPressed) {
+            if(keyCode === UP) {
+                y -= 1;
+            }
+            if(keyCode === DOWN) {
+                y += 1;
+            }
+            if(keyCode === LEFT) {
+                x -= 1;
+            }
+            if(keyCode === RIGHT) {
+                x += 1;
+            }
+        }
+    };
+}};
 
 var canvas = document.getElementById("mycanvas"); 
 var processingInstance = new Processing(canvas, programCode); 
